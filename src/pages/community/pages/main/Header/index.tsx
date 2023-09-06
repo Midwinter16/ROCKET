977f9df67ed4e26f018f00e2c4d7889b257569d0
Laulou <1053374792@qq.com>
2023-09-06 13:39:37 +0800

    community login page

--- src/pages/community/pages/main/Header/index.tsx
+++ src/pages/community/pages/main/Header/index.tsx
@@ -3,6 +3,7 @@ import SIcon from "@/components/SIcon";
 import { HeaderBannerList } from "@/pages/community/constants";
 import { Button, Col, Input, Row, Space } from "antd";
 import { useState } from "react";
+import { history } from "umi";
 import vipIcon from "../../../assets/imgs/vip.svg";
 import styles from "./index.less";
 
@@ -44,7 +45,13 @@ const Header = () => {
           <div className={styles["header-user"]}>
             <Space size={"large"}>
               <SIcon icon={vipIcon} size="small" title="会员"></SIcon>
-              <Button>注册 ｜ 登录</Button>
+              <Button
+                onClick={() => {
+                  history.push("/community/login");
+                }}
+              >
+                注册 ｜ 登录
+              </Button>
             </Space>
           </div>
         </Col>
