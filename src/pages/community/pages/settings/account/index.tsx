4fd050eb58698d1c224e54024382c96699e95866
Laulou <1053374792@qq.com>
2023-09-19 17:36:15 +0800

    init user and settings pages

---
+++ src/pages/community/pages/settings/account/index.tsx
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+// import styles from "./index.less";
+
+
+const Account = () => {
+  return (
+    <>
+      <div>
+        <h1>账号设置</h1>
+      </div>
+    </>
+  )
+}
+
+export default Account;
